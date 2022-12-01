8394abed09bbf4faca8728a2c54e95ada46aa175
quanjindeng <54259993+quanjindeng@users.noreply.github.com>
2022-12-01 21:58:39 +0800

    Update diy-imm-x86.sh

--- diy-imm-x86.sh
+++ diy-imm-x86.sh
@@ -69,4 +69,4 @@ sed -i 's#mount -t cifs#mount.cifs#g' feeds/luci/applications/luci-app-cifs-moun
 
 # 为alist插件更换最新的golang版本
 rm -rf feeds/packages/lang/golang
-svn co https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
