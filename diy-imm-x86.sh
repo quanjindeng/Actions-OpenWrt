ad998b66f1932bcc594030e960f9bdf27dd069c3
quanjindeng <54259993+quanjindeng@users.noreply.github.com>
2022-12-01 12:00:18 +0800

    Add files via upload

--- diy-imm-x86.sh
+++ diy-imm-x86.sh
@@ -37,6 +37,8 @@ svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-unblocknetease
 
 # 删除重复包
 
+rm -rf package/feeds/packages/naiveproxy
+rm -rf feeds/packages/net/naiveproxy
 # rm -rf feeds/luci/applications/luci-app-netdata
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf package/small-package/luci-app-openvpn-server
@@ -66,5 +68,5 @@ sed -i 's#mount -t cifs#mount.cifs#g' feeds/luci/applications/luci-app-cifs-moun
 #sed -i 's#<%+cbi/tabmenu%>##g' package/small-packages/luci-app-nginx-manager/luasrc/view/nginx-manager/index.htm
 
 # 为alist插件更换最新的golang版本
-# rm -rf feeds/packages/lang/golang
-# svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
